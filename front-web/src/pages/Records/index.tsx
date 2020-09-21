5355f84ed5b4b63dd66ec12258ae51692869a239
leonevil <leongb66@gmail.com>
2020-09-20 22:41:55 -0300

    Front-web configuration for production

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import './styles.css'
 import { RecordsResponse } from './types';
-import { formatDate } from './helpers'
+import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters'
 
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-leon.herokuapp.com';
 
 const Records = () => {
 
