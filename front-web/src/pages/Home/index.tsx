bf7c01cb57f7baddba0096701f27dd216da4de4b
lucas-serafim <lucas.serafim159@gmail.com>
2020-10-25 11:37:00 -0300

    some changes and new img directory

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -8,17 +8,17 @@ const Home = () => (
     <div className = "home-container">
         <div className = "home-text">
             <h1 className = "home-text-title">
-                Which games do the guys like the most?
+                Quais jogos a galera gosta mais?
             </h1>
 
             <h3 className = "home-text-subtitle">
-                Click on the button below and find out which games the players are playing!
+                Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
             </h3>
 
             <Link to = "/records">
                 <div className = "home-actions">
                     <button className = "home-btn">
-                        I WANT TO KNOW MORE
+                        QUERO SABER QUAIS SÃO
                     </button>
 
                     <div className = "home-btn-icon">
